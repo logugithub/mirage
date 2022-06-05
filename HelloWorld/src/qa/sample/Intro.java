6c7cca63a5be4182df15540f563f7eb59ef10618
logugithub <logeshkumarbook@gmail.com>
2022-06-05 12:29:04 +0530

    method commut

--- HelloWorld/src/qa/sample/Intro.java
+++ HelloWorld/src/qa/sample/Intro.java
@@ -2,13 +2,21 @@ package qa.sample;
 
 public class Intro {
 	int b = 23;
-	public void add(int a){
-	int c=a+b;
-	System.out.println(c);	
+
+	public void add(int a) {
+		int c = a + b;
+		System.out.println(c);
 	}
+
+	public void sub() {
+		int d = b - b;
+		System.out.println(d);
+	}
+
 	public static void main(String[] args) {
 		Intro i = new Intro();
 		i.add(2);
+		i.sub();
 		System.out.println("hii");
 	}
 }
