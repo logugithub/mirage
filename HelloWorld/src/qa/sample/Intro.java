7fac2fa405216ca5c37c0b757158f912b37450ed
logesh <98604782+logugithub@users.noreply.github.com>
2022-06-05 12:21:55 +0530

    new commit add

--- HelloWorld/src/qa/sample/Intro.java
+++ HelloWorld/src/qa/sample/Intro.java
@@ -1,7 +1,14 @@
 package qa.sample;
 
 public class Intro {
+	int b = 23;
+	public void add(int a){
+	int c=a+b;
+	System.out.println(c);	
+	}
 	public static void main(String[] args) {
+		Intro i = new Intro();
+		i.add(2);
 		System.out.println("hii");
 	}
 }
